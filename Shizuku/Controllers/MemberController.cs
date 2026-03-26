584e8b6232f75d5161c4b7ea283d4e794469ef68
AnnZi <56986861+SinSean@users.noreply.github.com>
2026-03-26 10:22:55 +0800

    Revert "Test member"

--- Shizuku/Controllers/MemberController.cs
+++ Shizuku/Controllers/MemberController.cs
@@ -16,17 +16,7 @@ namespace Shizuku.Controllers
 
             DbShizukuDemoContext db = new DbShizukuDemoContext();
 
-            IQueryable<TMember> query = db.TMembers.Where(p => p.FIsActive == true);
-
-            if (!string.IsNullOrEmpty(vm.txtKeyword))
-            {
-                string k = vm.txtKeyword;
-                // 搜尋：姓名、Email、電話 或 會員編號 (模糊比對)
-                query = query.Where(p => p.FName.Contains(k)
-                                      || p.FEmail.Contains(k)
-                                      || p.FPhone.Contains(k)
-                                      || p.FMemberId.Contains(k));
-            }
+            var query = db.TMembers;
 
             List<CMemberWrap> datas = new List<CMemberWrap>();
             foreach (var p in query)
@@ -39,20 +29,9 @@ namespace Shizuku.Controllers
         public IActionResult Block_List(CKeywordViewModel vm)
         {
             DbShizukuDemoContext db = new DbShizukuDemoContext();
-            IQueryable<TMember> query = db.TMembers.Where(p => p.FIsActive == false);
-            if (!string.IsNullOrEmpty(vm.txtKeyword))
-            {
-                string k = vm.txtKeyword;
-                query = query.Where(p => p.FName.Contains(k)
-                                      || p.FEmail.Contains(k)
-                                      || p.FPhone.Contains(k)
-                                      || p.FMemberId.Contains(k));
-            }
-            List<CMemberWrap> datas = new List<CMemberWrap>();
-            foreach (var p in query)
-            {
-                datas.Add(new CMemberWrap { member = p });
-            }
+            IEnumerable<TMember> datas = null;
+            datas = from p in db.TMembers
+                    select p;
             return View(datas);
         }
         public IActionResult Delete(int? id)
